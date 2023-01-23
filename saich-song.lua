382eab0b8805aada3e433e2a336dcae8c1254ba9
Zack Scholl <zack.scholl@gmail.com>
2023-01-23 23:53:37 +0000

    many fixes

--- saich-song.lua
+++ saich-song.lua
@@ -18,9 +18,47 @@ local select_param=1
 local select_possible={{"chord_beats","stay_on_chord"},{"movement_left","movement_right"}}
 local select_chord=1
 
+
+
+History = {}
+
+function History.new(max_size)
+   local hist = { __index = History }
+   setmetatable(hist, hist)
+   hist.max_size = max_size
+   hist.size = 0
+   hist.cursor = 1
+   return hist
+end
+
+function History:push(value)
+  if self.size < self.max_size then
+    table.insert(self, value)
+    self.size = self.size + 1
+  else
+    self[self.cursor] = value
+    self.cursor = self.cursor % self.max_size + 1
+  end
+end
+
+function History:iterator()
+  local i = 0
+  return function()
+    i = i + 1
+    if i <= self.size then
+      return i,self[(self.cursor - i - 1) % self.size + 1]
+    end
+  end
+end
+
+local marquee_chord=History.new(10)
+local marquee_note=History.new(10)
+
 function init()
+  print("saich song")
+  
   -- initialize arrays
-  for i=1,total_chords
+  for i=1,total_chords do
     table.insert(song_chord_notes,0)
     table.insert(song_chord_quality,"major")
   end
@@ -28,10 +66,10 @@ function init()
   local params_menu={
     {id="chord",name="chord",min=1,max=7,exp=false,div=1,default=1,formatter=function(param) return song_chord_possibilities[param:get()] end},
     {id="root",name="root",min=1,max=120,exp=false,div=1,default=48,formatter=function(param) return musicutil.note_num_to_name(param:get(),true)end},
-    {id="chord_beats",name="chord beats",min=1,max=32,exp=false,div=1,default=8,unit="beats"},
-    {id="stay_on_chord",name="stay on chord",min=0,max=1,exp=false,div=0.01,default=0.95,formatter=function(param) return string.format("%d",math.floor(params:get()*100)) end},
-    {id="movement_left",name="movement left",min=0,max=12,exp=false,div=1,default=6,unit="notes"},
-    {id="movement_right",name="movement right",min=0,max=12,exp=false,div=1,default=6,unit="notes"},
+    {id="chord_beats",name="chord beats",min=1,max=32,exp=false,div=1,default=8,formatter=function(param) return string.format("%d beats",math.floor(param:get())) end},
+    {id="stay_on_chord",name="stay on chord",min=0,max=1,exp=false,div=0.01,default=0.95,formatter=function(param) return string.format("%d%%",math.floor(param:get()*100)) end},
+    {id="movement_left",name="movement left",min=0,max=12,exp=false,div=1,default=6,formatter=function(param) return string.format("<- %d",math.floor(param:get())) end},
+    {id="movement_right",name="movement right",min=0,max=12,exp=false,div=1,default=6,formatter=function(param) return string.format("%d ->",math.floor(param:get())) end},
   }
   for i=1,total_chords do 
     for _,pram in ipairs(params_menu) do
@@ -42,7 +80,7 @@ function init()
           controlspec=controlspec.new(pram.min,pram.max,pram.exp and "exp" or "lin",pram.div,pram.default,pram.unit or "",pram.div/(pram.max-pram.min)),
           formatter=pram.formatter,
         }
-        if pramd.id=="chord" then 
+        if pram.id=="chord" then 
           params:set_action(pram.id..i,function(x)
             local v=params:string("chord"..i)
             song_chord_notes[i]=musicutil.generate_chord_roman(song_root,1,v)
@@ -54,12 +92,15 @@ function init()
   -- default chords
   for i, v in ipairs({1,6,4,3}) do 
     params:set("chord"..i,v)
+    params:set("stay_on_chord"..i,math.random(90,100)/100)
+    params:set("movement_left"..i,math.random(1,9))
+    params:set("movement_right"..i,math.random(1,9))
   end
 
 
   local song_melody_notes={}
-  local beat_chord=0
-  local beat_chord_index=0
+  local beat_chord=params:get("chord_beats"..total_chords)
+  local beat_chord_index=total_chords
   local beat_melody=0
   local beat_last_note=0
   clock.run(function()
@@ -75,45 +116,66 @@ function init()
               local movement_right={}
               local stay_on_chord={}
               local song_chords={}
+              local chord_beats={}
               local total_song_beats=0
-              for i=1,total_chords
+              for i=1,total_chords do
                 total_song_beats=total_song_beats+params:get("chord_beats"..i)
               end
+              local song_root=params:get("root1") -- TODO fix this
               for i=1,4 do 
                 table.insert(song_chords,song_chord_possibilities[params:get("chord"..i)])
                 table.insert(movement_left,params:get("movement_left"..i))
                 table.insert(movement_right,params:get("movement_right"..i))
                 table.insert(stay_on_chord,params:get("stay_on_chord"..i))
+                table.insert(chord_beats,params:get("chord_beats"..i))
               end
-              song_melody_notes=generate_melody(total_song_beats,song_chords,song_root,movement_left,movement_right,stay_on_chord)
+              song_melody_notes=generate_melody(chord_beats,song_chords,song_root,movement_left,movement_right,stay_on_chord)
+              -- print("new melody:")
+              -- for i,v in ipairs(song_melody_notes) do
+              --   print(i,musicutil.note_num_to_name(v),true)
+              -- end
             end
-            dprint("melody",string.format("next chord: %s",song_chord_possibilities[params:get("chord"..beat_chord_index]))
+            dprint("melody",string.format("next chord: %s",song_chord_possibilities[params:get("chord"..beat_chord_index)]))
             -- new chord
-            crow.output[1].volts=(song_chord_notes[beat_chord_index][1]-24)/12
-            crow.output[2].volts=song_chord_quality[beat_chord_index]=="major" and 10 or 0 -- TODO check if this is accurate
+            -- crow.output[1].volts=(song_chord_notes[beat_chord_index][1]-24)/12
+            -- crow.output[2].volts=song_chord_quality[beat_chord_index]=="major" and 10 or 0 -- TODO check if this is accurate
             -- new chord envelope
             local attack=clock.get_beat_sec()
             local decay=clock.get_beat_sec()
             local hold_time=(params:get("chord_beats"..beat_chord_index)*0.5)*clock.get_beat_sec()
-            crow.output[3].action = string.format("{ to(0,0), to(10,%2.3f), to(7,%2.3f), to(0,%2.3f) }",attack,hold_time,decay)
-            crow.output[3]()
+            -- crow.output[3].action = string.format("{ to(0,0), to(10,%2.3f), to(7,%2.3f), to(0,%2.3f) }",attack,hold_time,decay)
+            -- crow.output[3]()
         end
+        marquee_chord:push(params:string("chord"..beat_chord_index))
 
         -- iterate melody
+        local note_next_name=""
         if next(song_melody_notes)~=nil then 
           beat_melody=beat_melody%#song_melody_notes+1
           local next_note=song_melody_notes[beat_melody]
           if beat_last_note~=next_note then 
-              crow.output[4].volts=(next_note-24)/12
+              -- crow.output[4].volts=(next_note-24)/12
               dprint("melody",string.format("next note: %d",next_note))
+              note_next_name=musicutil.note_num_to_name(next_note,true)
           end
           beat_last_note=next_note
         end
+        marquee_note:push(note_next_name)
+        dprint("clock_run",string.format("chord beat %d, melody beat %d",beat_chord,beat_melody))
+
+    end
+  end)
+  
+  clock.run(function()
+    while true do
+      clock.sleep(1/10)
+      redraw()
     end
   end)
 end
 
-function generate_melody(total_beats,chord_structure,root_note,move_left,move_right,stay_scale)
+function generate_melody(beats_per_chord,chord_structure,root_note,move_left,move_right,stay_scale)
+  dprint("generate_melody",beats_per_chord,chord_structure,root_note,move_left,move_right,stay_scale)
   local factor=1
 
   -- notes to play
@@ -121,9 +183,13 @@ function generate_melody(total_beats,chord_structure,root_note,move_left,move_ri
 
   -- generate chords
   local chords={}
+  local scale=musicutil.generate_scale(12,1,8)
+  local note_start=0
   for i,v in ipairs(chord_structure) do
-    local scale=musicutil.generate_scale(12,1,8)
-    local chord_notes=musicutil.generate_chord_roman(root_note,1,song_chord_possibilities[v])
+    local chord_notes=musicutil.generate_chord_roman(root_note,1,v)
+    if i==1 then 
+      note_start=chord_notes[1]
+    end
     local notes_in_chord={}
     for _,u in ipairs(chord_notes) do
       notes_in_chord[u]=true
@@ -131,8 +197,7 @@ function generate_melody(total_beats,chord_structure,root_note,move_left,move_ri
         notes_in_chord[u+(12*j)]=true
       end
     end
-    local note_start=chord_notes[1]
-    for jj=1,4*factor do
+    for jj=1,beats_per_chord[i] do
       -- find note_start in scale
       local notes_to_choose={}
       for _,note in ipairs(scale) do
@@ -142,8 +207,8 @@ function generate_melody(total_beats,chord_structure,root_note,move_left,move_ri
       end
       local weights={}
       local scale_size=#notes_to_choose
-      for i,note in ipairs(notes_to_choose) do
-        weights[i]=notes_in_chord[note]~=nil and scale_size or scale_size*(1-util.clamp(stay_scale[i],0,1))
+      for notei,note in ipairs(notes_to_choose) do
+        weights[notei]=notes_in_chord[note]~=nil and scale_size or scale_size*(1-util.clamp(stay_scale[i],0,1))
         -- weights[i]=weights[i]+(scale_size-i)
       end
       local note_next=choose_with_weights(notes_to_choose,weights)
@@ -151,7 +216,7 @@ function generate_melody(total_beats,chord_structure,root_note,move_left,move_ri
       note_start=note_next
     end
   end
-
+  
   return notes_to_play
 end
 
@@ -197,9 +262,19 @@ function redraw()
     screen.text_center(params:string("chord"..i))
   end
   for i,v in ipairs(select_possible) do 
-    screen.move(64,20+12*i)
     screen.level(select_param==i and 15 or 5)
-    screen.text_center(string.format("%s: %s    %s: %s",v[1],params:string(v[1]..select_chord,v[2],params:string(v[2]..select_chord))))
+    screen.move(32,10+12*i)
+    screen.text_center(string.format("%s",params:string(v[1]..select_chord)))
+    screen.move(96,10+12*i)
+    screen.text_center(string.format("%s",params:string(v[2]..select_chord)))
+  end
+  for i,v in marquee_chord:iterator() do 
+      screen.move(128-i*12,50)
+      screen.text(v)
+  end
+  for i,v in marquee_note:iterator() do 
+      screen.move(128-i*12,60)
+      screen.text(v)
   end
   screen.update()
 end
